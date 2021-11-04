0213fcfba41cc2f75b1a3b628708a53c95c19abd
Peter <pete225848@gmail.com>
2021-11-04 13:14:28 -0700

    created Article component

--- src/services/fetchNewsAPI.js
+++ src/services/fetchNewsAPI.js
@@ -1,14 +1,14 @@
 export const fetchNewsAPI = async () => {
   const res = await fetch(
-    'https://newsapi.org/v2/top-headlines?country=us&apiKey=0c15ba5dbff749cdb26f00a8891735de'
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_API_KEY}`
   );
 
   const json = await res.json();
   return json.articles.map((article) => ({
-    author: article.author,
     title: article.title,
-    description: article.description,
-    url: article.url,
+    author: article.author,
     image: article.urlToImage,
+    url: article.url,
+    description: article.description,
   }));
 };
